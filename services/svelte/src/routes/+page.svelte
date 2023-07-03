8e710324f62dd5ec24c59d2384dba13efd9c20b8
Christophe Cauet <ccauet@point-8.de>
2023-07-03 21:42:18 +0200

    Simple svelte app that requests data from the example fastapi backend service

--- services/svelte/src/routes/+page.svelte
+++ services/svelte/src/routes/+page.svelte
@@ -1,2 +1,24 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<script>
+  let clicked = false;
+  let item;
+
+	async function handleClick() {
+    clicked = true;
+
+    item = await fetch(`https://localhost/api/random`).then((r) => r.json());
+    console.log(item)
+	}
+</script>
+
+<button on:click|once={handleClick}> Click me </button>
+
+{@debug item}
+
+{#if clicked}
+	<p>Connecting to backend...</p>
+
+  <p>{item}</p>
+{/if}
