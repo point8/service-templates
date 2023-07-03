0a4b86ed750a54112782ff55cf87dd38aec27d89
Christophe Cauet <ccauet@point-8.de>
2023-07-03 16:39:45 +0200

    Add random int endpoint to fastapi backend

--- services/fastapi/example.py
+++ services/fastapi/example.py
@@ -1,21 +1,22 @@
 # Credit and Disclaimer:
-# This example was copied from https://fastapi.tiangolo.com/#example
-# 2022-07-13
+# This example was copied from https://fastapi.tiangolo.com/#example and slighty
+# adapted for our own needs
+# - 2022-07-13
 #
 # The MIT License (MIT)
-# 
+#
 # Copyright (c) 2018 Sebastián Ramírez
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be included in
 # all copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -24,6 +25,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
+import random
 from typing import Union
 
 from fastapi import FastAPI
@@ -34,6 +36,11 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+
+@app.get("/random")
+def random():
+    return [random.randrange(10) for i in range(10)]
 
 
 @app.get("/items/{item_id}")
